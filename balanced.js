bc758b95ca7c77fcd0c6ac74db90201c7a8b87aa
nathan <c-nathan.rogers@charter.com>
2018-09-22 19:56:32 -0600

    is scheme so good compared to this?

---
+++ balanced.js
@@ -0,0 +1,11 @@
+const isMatchedClosing = (code, stack) => stack.length &&
+      stack[stack.length - 1] === String.fromCharCode(code - 1) ||
+      stack[stack.length - 1] === String.fromCharCode(code - 2)
+
+const isBalanced = (string, stack=[], open='({[') => !Boolean(
+    string.split('').map(c => {
+	const code = c.charCodeAt(0)
+	return open.includes(c) ? stack.push(c)
+	    : isMatchedClosing(code, stack) ? stack.pop()
+	    : false
+    }).some(x => x === false) || stack.length)
